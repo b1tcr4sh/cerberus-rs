c1c88c7a28d0f6e55c0cc6a63d620667cb7a43a9
Ethan Ford <ethan.ford06@gmail.com>
2023-02-14 21:45:05 -0600

    Compiler big angry

--- src/commands/util.rs
+++ src/commands/util.rs
@@ -7,22 +7,18 @@ use serenity::json::prelude::from_str;
 
 use crate::reaction_handler::{ReactionHandler};
 
-#[command]
+#[command("reaction-role")]
 #[num_args(3)]
-#[usage("reaction [Message ID]<string> [Role ID]<string> [Emoji Name]<string>")]
+#[usage("reaction [Message ID]<string> [Role ID]<string>")]
 async fn reaction(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let mut args_vec: Vec<String> = Vec::new(); 
     
     let Some(message_id) = args.current() else {
-        msg.reply(&ctx, "Missing message ID ~~bitch~~");
+        msg.reply(ctx, "Missing message ID ~~bitch~~");
         return;
     };
     let Some(role_id) = args.advance().current() else {
-        msg.reply(&ctx, "Missing role ID ~~bitch~~");
-        return;
-    };
-    let Some(emoji_name) = args.advance().current() else {
-        msg.reply(&ctx, "Missing emoji name ~~bitch~~");
+        msg.reply(ctx, "Missing role ID ~~bitch~~");
         return;
     };
 
@@ -32,16 +28,16 @@ async fn reaction(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     let guild_id = msg.guild_id.expect("Failed to get guild id from message");
     let roles = ctx.cache.guild_roles(guild_id).expect("Failed to get roles from cache");
 
-    let lock_guard = &ctx.data.read().await;
+    let lock_guard = ctx.data.read().await;
     let reaction_handler = lock_guard.get::<ReactionHandler>().expect("Reaction manager from r/w lock");
 
-    let reaction_request = &msg.reply(&ctx, "React to this message with the emoji you wish to use").await.unwrap();
+    let reaction_request = msg.reply(ctx, "React to this message with the emoji you wish to use").await.unwrap();
     reaction_handler.active_creation_listener = Some(reaction_request.id);
 
     let timeout = 0;
     while reaction_handler.paylod() == None {
         if timeout > 10 {
-            reaction_request.edit(&ctx, |m| m.content("You took too long, sorry (10 secs timeout)"));
+            reaction_request.edit(ctx, |m| m.content("You took too long, sorry (10 secs timeout)"));
 
             reaction_handler.active_creation_listener = None;
             return;
@@ -52,28 +48,17 @@ async fn reaction(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     }
 
     let Some(reaction) = reaction_handler.paylod() else {
-        msg.reply(&ctx, "Something went wrong, I don't have a fucking clue.");
+        msg.reply(ctx, "Something went wrong, I don't have a fucking clue.");
         return;
     };
 
 
-    // let timeout = 0;
-    // while reaction_handler.active_creation_listener == None {
-    //     if timeout >= 20 {
-    //         reaction_request.edit(&ctx, |m| m.content("You took too long bitch.  I timed out (10 seconds)"));
-    //         return;
-    //     }
-        
-    //     sleep(time::Duration::from_secs(1));
-    //     timeout += 1;
-    // }
-
     let Ok(message_id_u64) = from_str::<u64>(message_id) else {
-        &msg.reply(&ctx, "Your first arg is wrong bitch");
+        msg.reply(ctx, "Your first arg is wrong bitch");
         return;
     };
     let Ok(role_id_u64) = from_str::<u64>(role_id) else {
-        &msg.reply(&ctx, "Your second arg is wrong bitch");
+        msg.reply(ctx, "Your second arg is wrong bitch");
         return;
     };
 
